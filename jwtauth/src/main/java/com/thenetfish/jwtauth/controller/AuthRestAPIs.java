433583fa1c76a79bc68b0b24d24cf38fb76b5d52
Davide Rubiu <87277751+Brrake@users.noreply.github.com>
2022-01-08 15:56:23 +0100

    BackendSpring JWT

---
+++ jwtauth/src/main/java/com/thenetfish/jwtauth/controller/AuthRestAPIs.java
@@ -0,0 +1,83 @@
+package com.thenetfish.jwtauth.controller;
+
+import javax.validation.Valid;
+
+import com.thenetfish.jwtauth.security.services.UserPrinciple;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import com.thenetfish.jwtauth.message.request.LoginForm;
+import com.thenetfish.jwtauth.message.request.SignUpForm;
+import com.thenetfish.jwtauth.message.response.JwtResponse;
+import com.thenetfish.jwtauth.model.User;
+import com.thenetfish.jwtauth.repository.UserRepository;
+import com.thenetfish.jwtauth.security.jwt.JwtProvider;
+
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthRestAPIs {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    UserPrinciple userPrinciple;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateJwtToken(authentication);
+        return ResponseEntity.ok(new JwtResponse(jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody SignUpForm signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity<String>("Fail -> Username is already taken!",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity<String>("Fail -> Email is already in use!",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().body("User registered successfully!");
+    }
+    @GetMapping("/username/{username}")
+    public Optional<User> getUser(@PathVariable("username") String username) {
+        return userRepository.findByUsername(username);
+    }
+}
