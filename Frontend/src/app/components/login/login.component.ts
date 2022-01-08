a12f6bbf8626c0cd9f71614b5dccad98f20c59f5
Davide Rubiu <87277751+Brrake@users.noreply.github.com>
2022-01-08 15:50:42 +0100

    Modifica file ts della pagina di login

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Router, NavigationEnd } from '@angular/router';
-import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../jwt-auth/auth/auth.service';
+import { TokenStorageService } from '../../jwt-auth/auth/token-storage.service';
+import { AuthLoginInfo } from '../../jwt-auth/auth/login-info';
 
 
 @Component({
@@ -9,16 +10,69 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
-
 export class LoginComponent implements OnInit {
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+  private loginInfo: AuthLoginInfo;
+  public username$: String;
+  public userLoggedId$: String;
+  public userId: number;
 
 
+  constructor(private authService: AuthService, public tokenStorage: TokenStorageService) {
+    const username = sessionStorage.getItem('usernameLogged');
+    this.username$ = username;
+    const UserId = sessionStorage.getItem('customer_id');
+    this.userLoggedId$ = UserId;
+    var userIdString = sessionStorage.getItem("customer_id"); ///Get value as string
+    this.userId = parseInt(userIdString)//Returns userId in number
+  }
 
-  constructor(public authService: AuthService) {  }
+  ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.authService.getUserIdByUsername(this.username$).subscribe(response => {
+        this.tokenStorage.saveUserId(response.id)
+      }
+      )
+    }
+
+  }
+
+  onSubmit() {
+        this.loginInfo = new AuthLoginInfo(
+      this.form.username,
+      this.form.password);
 
 
-  ngOnInit(): void {
-    
+    this.authService.attemptAuth(this.loginInfo).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUsername(data.username);
+        window.sessionStorage.setItem('usernameLogged', this.form.username)
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+
+        this.reloadPage();
+      },
+      error => {
+
+        this.errorMessage = error.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
+  logout() {
+    this.tokenStorage.signOut();
+    this.reloadPage();
   }
 }
+
