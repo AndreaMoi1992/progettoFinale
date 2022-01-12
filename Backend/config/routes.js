0acde2d0fc2682a324d91583366a5dd61faf17ff
Davide Rubiu <87277751+Brrake@users.noreply.github.com>
2022-01-12 16:42:14 +0100

    Adattamento backend alla rimozione di data

--- Backend/config/routes.js
+++ Backend/config/routes.js
@@ -1,29 +1,11 @@
-const DataEngine = require('../engine/entry');
-const UserEngine = require('../routes/userEntry');
 const MovieDatabaseEngine = require('../routes/moviesDatabaseEntry');
 
 const ErrorsEngine  = require('../engine/errors');
 
 module.exports = (app) => {
 
-  const dataPath = '/data';
-  const dataUserPath = '/user';
+
   const movieDatabasePath = '/movieDatabase';
-
-
-  /********** DATA REST APIs **********/
-  app.get(dataPath, DataEngine.getEntry);
-  app.post(dataPath, DataEngine.createEntry);
-  app.get(`${dataPath}/:id`, DataEngine.getEntryById);
-  app.put(`${dataPath}/:id`, DataEngine.editEntry);
-  app.delete(`${dataPath}/:id`, DataEngine.deleteEntry);
-
-    /********** USER REST APIs **********/
-    app.get(dataUserPath, UserEngine.getUserEntry);
-    app.post(dataUserPath, UserEngine.createUserEntry);
-    app.get(`${dataUserPath}/:id`, UserEngine.getUserEntryById);
-    app.put(`${dataUserPath}/:id`, UserEngine.editUserEntry);
-    app.delete(`${dataUserPath}/:id`, UserEngine.deleteUserEntry);
 
     /********** MOVIE DATABASE REST APIs **********/
     app.get(movieDatabasePath, MovieDatabaseEngine.getMovieDatabaseEntry);
