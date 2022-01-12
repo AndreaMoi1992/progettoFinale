da29496b0a20c2e9d55e79b48df67f84eff6e530
Davide Rubiu <87277751+Brrake@users.noreply.github.com>
2022-01-12 16:35:23 +0100

    Rimozione file superflui

--- Frontend/src/app/pipes/genrepipe/genrepipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GenrepipePipe } from './genrepipe.pipe';
-
-describe('GenrepipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GenrepipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
